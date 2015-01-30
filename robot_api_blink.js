caf9e5fc4d6f9639e8b63627e5680f4eb74f2387
David Larrimore <davidlarrimore@gmail.com>
2015-01-30 13:17:09 -0500

    ;j

--- robot_api_blink.js
+++ robot_api_blink.js
@@ -3,7 +3,7 @@ var Cylon = require('cylon');
 // ensure you install the API plugin first:
 // $ npm install cylon-api-http
 Cylon.api({
-  host: "127.0.0.1",
+  host: "0.0.0.0",
   port: "8888"
 });
 
